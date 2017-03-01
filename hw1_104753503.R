6728c4aa9d0afc5ab3c97ab87e6ecf4cbac2e70c
winterthink <winterthink@gmail.com>
2017-03-02 00:47:34 +0800

    Import data from web link(RAW link)

--- hw1_104753503.R
+++ hw1_104753503.R
@@ -1,8 +1,10 @@
-filepath = ("/Users/jimmyhsu/Desktop/NCCU_1052_DataScience/test.1.csv")
-hw1 <- read.csv(filepath)
+require(RCurl)
+URL <- "https://raw.githubusercontent.com/winterthink/NCCU_1052_DataScience_104753503/master/test.1.csv"
+hw1 <- getURL(URL)
+hw1 <- read.csv(textConnection(hw1))
 weight = round(max(hw1$weight),2)
 height = round(max(hw1$height),2)
-tools::file_path_sans_ext(basename(filepath))
+set <- tools::file_path_sans_ext(basename(URL)) #get filename without ext
 result = c("set", colnames(hw1[2:3]), set,weight, height)
 matrix(result, nrow = 2, ncol = 3, byrow = T)
-write.table(matrix(result, nrow = 2, ncol = 3, byrow = T), file = "104753503_27.csv",sep = ",", row.names = F, col.names = F)
+write.table(matrix(result, nrow = 2, ncol = 3, byrow = T), file = "104753503_result.csv",sep = ",", row.names = F, col.names = F)
