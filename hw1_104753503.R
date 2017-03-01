f56b64f378b011b7dfbe876051f57212baa021da
winterthink <winterthink@gmail.com>
2017-03-01 16:50:34 +0800

    Add files via upload

---
+++ hw1_104753503.R
@@ -0,0 +1,8 @@
+filepath = ("/Users/jimmyhsu/Desktop/NCCU_1052_DataScience/test.1.csv")
+hw1 <- read.csv(filepath)
+weight = round(max(hw1$weight),2)
+height = round(max(hw1$height),2)
+tools::file_path_sans_ext(basename(filepath))
+result = c("set", colnames(hw1[2:3]), set,weight, height)
+matrix(result, nrow = 2, ncol = 3, byrow = T)
+write.table(matrix(result, nrow = 2, ncol = 3, byrow = T), file = "104753503_27.csv",sep = ",", row.names = F, col.names = F)
