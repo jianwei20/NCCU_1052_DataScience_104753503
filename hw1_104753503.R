cfcb95c120b181421e766f124c567bfdca7ae084
winterthink <winterthink@gmail.com>
2017-03-13 19:57:54 +0800

    Update hw1_104753503.R

--- hw1_104753503.R
+++ hw1_104753503.R
@@ -1,10 +1,22 @@
-require(RCurl)
-URL <- "https://raw.githubusercontent.com/winterthink/NCCU_1052_DataScience_104753503/master/test.1.csv"
-hw1 <- getURL(URL)
-hw1 <- read.csv(textConnection(hw1))
-weight = round(max(hw1$weight),2)
-height = round(max(hw1$height),2)
-set <- tools::file_path_sans_ext(basename(URL)) #get filename without ext
-result = c("set", colnames(hw1[2:3]), set,weight, height)
+args = commandArgs(trailingOnly=TRUE) 
+if (length(args)==0) {
+  stop("USAGE: Rscript hw1_104753503.R input", call.=FALSE) 
+} 
+i <- 1
+while (i< length(args)){
+ if (args[i] == "-files") {
+  inputfile <- args[i+1]
+  i <- i+1
+ } else if (args[i] == "-out"){
+   outputfile <- args[i+1]
+   i <- i+1
+ }
+i <- i+1
+}
+hw1 <- read.csv(inputfile)
+weight <- round(max(hw1$weight),2)
+height <- round(max(hw1$height),2)
+result_set <- tools::file_path_sans_ext(basename(inputfile)) #get filename without ext
+result = c("set", colnames(hw1[2:3]), result_set,weight, height)
 matrix(result, nrow = 2, ncol = 3, byrow = T)
-write.table(matrix(result, nrow = 2, ncol = 3, byrow = T), file = "104753503_result.csv",sep = ",", row.names = F, col.names = F)
+write.table(matrix(result, nrow = 2, ncol = 3, byrow = T), file = outputfile ,sep = ",", row.names = F, col.names = F)
